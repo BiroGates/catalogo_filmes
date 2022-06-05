0de2742ac5cff6f0b8b0bd2be4e6676cd79a8cf3
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-06-05 16:22:26 -0300

    video 5

--- site/src/components/menu/index.js
+++ site/src/components/menu/index.js
@@ -4,27 +4,20 @@ import { useNavigate, Link } from 'react-router-dom';
 import './index.scss'
 
 export default function Index({ selecionado }) {
-    const [menuSelecionado, setMenuSelecionado] = useState(selecionado);
     const navigate = useNavigate();
     
     function sairClick() {
         storage.remove('usuario-logado');
         navigate('/');
     }
-
-    function selectionarMenu(menu) {
-        setMenuSelecionado(menu);
-    }
     
     function verificarMenuSelecionado(menu) {
-        if(menu == menuSelecionado){
+        if(menu === selecionado){
             return 'selecionado'
         }else{
             return '';
         }
     }
-    
-    console.log(menuSelecionado);
 
     return (
         <nav className="comp-menu">
@@ -35,15 +28,15 @@ export default function Index({ selecionado }) {
                 </div>
 
                 <div className='menu-items'>
-                    <Link to='/admin' onClick={() => selectionarMenu('home')} className={verificarMenuSelecionado('home') }>
+                    <Link to='/admin' className={verificarMenuSelecionado('home') }>
                         <img src="/assets/images/icon-home.svg" alt="home" />
                         <div>Home</div>
                     </Link>
-                    <Link to='/admin/cadastrar' onClick={() => selectionarMenu('cadastrar') } className={verificarMenuSelecionado('cadastrar') }>
+                    <Link to='/admin/cadastrar' className={verificarMenuSelecionado('cadastrar') }>
                         <img src="/assets/images/icon-cadastrar.svg" alt="cadastrar" />
                         <div>Cadastrar</div>
                     </Link>
-                    <Link to='/admin/consultar' onClick={() => selectionarMenu('consultar')} className={verificarMenuSelecionado('consultar') }>
+                    <Link to='/admin/consultar' className={verificarMenuSelecionado('consultar') }>
                         <img src="/assets/images/icon-consultar.svg" alt="consultar" />
                         <div>Consultar</div>
                     </Link>
